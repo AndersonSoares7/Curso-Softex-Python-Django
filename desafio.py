1e62332a94de9fe079486cd1813d2b45ccfa2a95
AndersonSoares7 <andersonpatrickb7@gmail.com>
2025-08-20 11:56:07 -0300

    Codigo da Padaria

--- desafio.py
+++ desafio.py
@@ -33,6 +33,36 @@ while True:
     else:
         print(f'Infelizmente só temos {quantidade_frances} no momento, ')
         break
-    forma_retirada = input(f'Qual o Bairro? (1:{bairro_barroco} ou 2:{bairro_saojose})? ')  
+
+    forma_retirada = input(f'Qual a forma de retirada? (1: retirar ou 2: entregar)? ')
+    if forma_retirada == '2':
+       bairro_entrega = input(f"Qual o bairro? (1:{bairro_barroco},2:{bairro_saojose})")
+    if bairro_entrega == '1':
+        valor_frete = frete_barroco
+    if bairro_entrega == '2':
+        print('Valor do frete R%{valor_frete}')
+    elif bairro_entrega == '2':
+        valor_frete = frete_saojose
+        print(f'Valor do frete R${valor_frete}')
+    else:
+        print('Fora da Área de entrega')
+        break
     if forma_retirada == '1':
+        valor_frete = 0.00
+    else:
+        break
+
+    dados_cliente = input("Digite seu nome: ")
+    forma_pagamento = input('Escolha forma de pagamento (1: dinheiro, 2: cartão): ')
+
+    if forma_pagamento == '1':
+        forma_pagamento = 'dinheiro'
+
+    
+    else:
+        forma_pagamento = 'cartão'
+
+    codigo_venda += 1
         
+    print(f'O valor total da sua compra é R${valor_compra + valor_frete}')
+    break
